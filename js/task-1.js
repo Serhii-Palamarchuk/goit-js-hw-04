2b7e0d7c1b300de9e7274ca8a8ee2d5da7af6a5c
Serhii-Palamarchuk <serhiy.palamarchuk@gmail.com>
2025-01-16 21:53:38 +0200

    Implement capacity check function for product packaging

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,23 @@
-
+/* Задача 1. Пакування товарів​*/
 
 'use strict';
 
 console.log("<=== TASK-1 ============================================>");
+
+function isEnoughCapacity(products, containerSize) {
+  let totalProducts = 0; 
+  // Ітеруємо по масиву значень об'єкта products і додаємо кожне значення до totalProducts
+  for (const quantity of Object.values(products)) {
+    totalProducts += quantity;
+  }
+
+  // Повертаємо результат перевірки
+  return totalProducts <= containerSize;
+}
+
+// Перевірка результатів
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+
